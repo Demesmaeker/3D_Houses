ccd0232c5ca42d1f8a8f4e2eda88b2ba72929a91
Morgane Demesmaeker <morgane.demesmaeker@gmail.com>
2020-10-08 15:32:58 +0200

    Highlight on 'no values data'

--- App/main.py
+++ App/main.py
@@ -1,16 +1,22 @@
-import numpy
-import pandas
-import rasterio
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import geopandas as gpd
+import rasterio as rio
 from rasterio.plot import show
+import earthpy as et
+import earthpy.plot as ep
 
 number = 5
 
 # DSM
-fp = f'App/Resources/Flandre/DHMVIIDSMRAS1m_k{number:02d}/GeoTIFF/DHMVIIDSMRAS1m_k{number:02d}.tif'
-img = rasterio.open(fp)
-show(img)
+dsm_path = f'F:/Resources/Flandre/DSM/DHMVIIDSMRAS1m_k{number:02d}/GeoTIFF/DHMVIIDSMRAS1m_k{number:02d}.tif'
+# img = rio.open(dsm_path)
+# show(img)
 
 # DTM
-# fp = r'App/Resources/01_DTM/GeoTIFF/01_DTM.tif'
-# img = rasterio.open(fp)
+dtm_path = f'F:/Resources/Flandre/DTM/DHMVIIDTMRAS1m_k{number:02d}/GeoTIFF/DHMVIIDTMRAS1m_k{number:02d}.tif'
+# img = rio.open(dtm_path)
 # show(img)
+
