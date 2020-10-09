30dd248e43a2ffed44eab5bb416cf70a1c956736
Morgane Demesmaeker <morgane.demesmaeker@gmail.com>
2020-10-09 15:11:12 +0200

    set pah for Walloonia

---
+++ App/functions.py
@@ -0,0 +1,7 @@
+def set_path_dsm(province):
+    wal_dsm = f"D:/Resources/Wallonia/DSM 2013-2014/RELIEF_WALLONIE_MNS_2013_2014_GEOTIFF_31370_PROV_{province}/RELIEF_WALLONIE_MNS_2013_2014.tif"
+    return wal_dsm
+
+def set_path_dtm(province):
+    wal_dtm = f"D:/Resources/Wallonia/DTM 2013-2014/RELIEF_WALLONIE_MNT_2013_2014_GEOTIFF_31370_PROV_{province}/RELIEF_WALLONIE_MNT_2013_2014.tif"
+    return wal_dtm
