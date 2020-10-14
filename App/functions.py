1231049cf155cdfc9a62ce6d8b7dd98350e233c0
Demesmaeker <morgane.demesmaeker@gmail.com>
2020-10-14 16:26:52 +0200

    Mayavi is Alive! ALIVE!

--- App/functions.py
+++ App/functions.py
@@ -1,7 +1,7 @@
 def set_path_dsm(province):
-    wal_dsm = f"D:/Resources/Wallonia/DSM 2013-2014/RELIEF_WALLONIE_MNS_2013_2014_GEOTIFF_31370_PROV_{province}/RELIEF_WALLONIE_MNS_2013_2014.tif"
+    wal_dsm = f"/home/morgane/Documents/Resources/Wallonia/DSM 2013-2014/RELIEF_WALLONIE_MNS_2013_2014_GEOTIFF_31370_PROV_{province}/RELIEF_WALLONIE_MNS_2013_2014.tif"
     return wal_dsm
 
 def set_path_dtm(province):
-    wal_dtm = f"D:/Resources/Wallonia/DTM 2013-2014/RELIEF_WALLONIE_MNT_2013_2014_GEOTIFF_31370_PROV_{province}/RELIEF_WALLONIE_MNT_2013_2014.tif"
+    wal_dtm = f"/home/morgane/Documents/Resources/Wallonia/DTM 2013-2014/RELIEF_WALLONIE_MNT_2013_2014_GEOTIFF_31370_PROV_{province}/RELIEF_WALLONIE_MNT_2013_2014.tif"
     return wal_dtm
